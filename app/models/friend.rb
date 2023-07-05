7fea35a1f3d422463c958f52bd5836887b24f60b
nirmalkr23 <nirmalworkspace@gmail.com>
2023-07-05 17:27:45 +0530

    validation for fields done

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,14 @@
 class Friend < ApplicationRecord
+    validates :first_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :last_name, presence: true
+    validates :phone_number, presence: true, length: { minimum: 10, maximum: 10 }
+
+    validate :CheckNamePresence
+
+    private
+    def CheckNamePresence
+        first_name=Friend.where("first_name =?",first_name)
+        errors.add(:first_name,"Already taken")
+    end
 end
